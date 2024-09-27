99818748bd820ab7238ba10dce4e1b1022ef3a62
Ng-DucM1nh <89336448+Ng-DucM1nh@users.noreply.github.com>
2024-09-27 15:24:53 +1000

    Update calibrate_for_freq.cpp

--- ELEC1601/calibrate_for_freq.cpp
+++ ELEC1601/calibrate_for_freq.cpp
@@ -1,20 +1,35 @@
+const int irLedPin=6, irReceiverPin=7;
+const int redLedPin = A1;
+
+void setup()
+{
+  delay(3000);
+  Serial.begin(9600);
+  Serial.println(find_freq_for(8));
+}
+
+void loop()
+{
+}
+
 int find_freq_for(int x)
 {
-    int ans = -1;
-    int current_freq = 38000;
-    while (current_freq <= 46000){
-        tone(irLedPin, current_freq);
-        delay(1);
-        noTone(irLedPin);
-        int ir = digitalRead(irReceiverPin);
-        if (ir == 0){
-            // wall detected
-            ans = current_freq;
-            current_freq += 100;
-        }
-        else{
-            // no wall detected
-            break;
-        }
-    }
+  int ans = -1;
+  long current_freq = 38000;
+  while (current_freq <= 46000){
+    Serial.println(current_freq);
+    tone(irLedPin, current_freq);
+    delay(1);
+  noTone(irLedPin);
+  int ir = digitalRead(irReceiverPin);
+  if (ir == 0){
+  // wall detected
+  ans = current_freq;
+  current_freq += 100;
+  }
+  else{
+  // no wall detected
+  break;
+  }
+  }
 }
