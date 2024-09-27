0643d3c87680745941732ef5f764cef73bdabed8
Ng-DucM1nh <89336448+Ng-DucM1nh@users.noreply.github.com>
2024-09-27 15:31:59 +1000

    Update calibrate_for_freq.cpp

--- ELEC1601/calibrate_for_freq.cpp
+++ ELEC1601/calibrate_for_freq.cpp
@@ -1,35 +1,42 @@
-const int irLedPin=6, irReceiverPin=7;
-const int redLedPin = A1;
+const int irLedPin = 10, irReceiverPin = 11;
+const int redLedPin = A2;
 
 void setup()
-{
-  delay(3000);
+{   
+  pinMode(irReceiverPin, INPUT);            // IR receiver pin is an input
+  pinMode(irLedPin, OUTPUT);                // IR LED pin is an ouput
+  pinMode(redLedPin, OUTPUT);               
   Serial.begin(9600);
-  Serial.println(find_freq_for(8));
+  Serial.println(find_freq_for(5));
 }
 
 void loop()
 {
+  
 }
 
-int find_freq_for(int x)
+long find_freq_for(int distance)
 {
-  int ans = -1;
+  long ans = -1;
   long current_freq = 38000;
+  Serial.println("initial frequency: ");
+  Serial.println(current_freq);
   while (current_freq <= 46000){
+    Serial.println("considering frequency :");
     Serial.println(current_freq);
     tone(irLedPin, current_freq);
     delay(1);
-  noTone(irLedPin);
-  int ir = digitalRead(irReceiverPin);
-  if (ir == 0){
-  // wall detected
-  ans = current_freq;
-  current_freq += 100;
+    noTone(irLedPin);
+    int ir = digitalRead(irReceiverPin);
+    if (ir == 0){
+      // wall detected
+      ans = current_freq;
+      current_freq += 100;
+    }
+    else{
+      // no wall detected
+      break;
+    }
   }
-  else{
-  // no wall detected
-  break;
-  }
-  }
+  return ans;
 }
